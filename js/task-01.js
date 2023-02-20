97e6e9e7fdffb475ac94dae221b0b66fb463377d
YuDo-fsd <y.doloban@gmail.com>
2023-02-20 15:27:51 +0100

    task3-checkpoint

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+// // знаходимо список категорій за його id
+// const categoriesList = document.querySelector("#categories");
+
+// // знаходимо всі елементи li з класом item в списку категорій
+// const categories = categoriesList.querySelectorAll("li.item");
+
+// // виводимо загальну кількість категорій
+// console.log(`Number of categories: ${categories.length}`);
+
+// // для кожної категорії виводимо текст заголовку та кількість підкатегорій
+// categories.forEach((category) => {
+//   const categoryName = category.querySelector("h2").textContent;
+//   const subcategories = category.querySelectorAll("li").length;
+//   console.log(`Category:${categoryName} Elements:${subcategories}`);
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIng = document.querySelector("#ingredients");
+
+const newLiElements = ingredients.map((ingredient) => {
+  const newLiCreate = document.createElement("li");
+  newLiCreate.textContent = ingredient;
+  newLiCreate.classList.add("item");
+  return newLiCreate;
+});
+
+listOfIng.append(...newLiElements);
