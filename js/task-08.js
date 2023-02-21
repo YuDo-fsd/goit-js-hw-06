b7da175bf2cd57338aa9c0e379bff0c492e97ccf
YuDo-fsd <y.doloban@gmail.com>
2023-02-21 23:39:07 +0100

    task8 - finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const users = {
+  login: [],
+  password: [],
+};
 
+const formEdit = document.querySelector(".login-form");
+const fieldsInput = document.querySelector("input");
+
+formEdit.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Будь ласка, заповніть всі поля форми!");
+  }
+
+  users.login.push(email.value);
+  users.password.push(password.value);
+
+  event.currentTarget.reset();
+}
+
+console.log(users);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const widgetBgColor = document.querySelector(".color");
+
+widgetBgColor.style.backgroundcolor = getRandomHexColor;
